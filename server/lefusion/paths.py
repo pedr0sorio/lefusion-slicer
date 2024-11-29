23adc3170c091527af52943f8761a5f026a6fc05
pedr0sorio <pedro.c.osorio@gamil.com>
2024-11-29 15:25:33 +0000

    add lidc preproc suite

--- server/lefusion/paths.py
+++ server/lefusion/paths.py
@@ -9,6 +9,7 @@ PROJECT_LEFUSION_PATH = Path(__file__).parent # lefusion-slicer/server/lefusion
 # ... Server Paths
 PROJECT_SERVER_PATH = PROJECT_ROOT_PATH / "server" # lefusion-slicer/server
 SERVER_DATA_DIR = PROJECT_SERVER_PATH / "data"
+TRAIN_DATA_DIR = PROJECT_SERVER_PATH / "train-data" # deafult pp train data folder
 IN_SERVER_DATA_DIR = SERVER_DATA_DIR / 'in'
 SERVER_ASSET_DATA_DIR = SERVER_DATA_DIR / 'in' / "mask-asset"
 OUT_SERVER_DATA_DIR = SERVER_DATA_DIR / 'out' # TODO ensure the out files are saved here
